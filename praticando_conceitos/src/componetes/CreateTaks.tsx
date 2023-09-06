d70acd5a83d38e5226193bc494ab880d679018bf
Jhonatan Souza <jhonatandesouzade13@gmail.com>
2023-09-06 16:59:44 -0300

    projeo 1 recebendo melhoria

--- praticando_conceitos/src/componetes/CreateTaks.tsx
+++ praticando_conceitos/src/componetes/CreateTaks.tsx
@@ -29,9 +29,7 @@ export default function CreateTaks({ create }: CreateTaksProps) {
             create(title)
             setTitle("")
         }
-        else {
-            alert("Não é permitido entrada de valor vazio!")
-        }
+        
 
     }
 
